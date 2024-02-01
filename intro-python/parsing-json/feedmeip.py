8a23b26ab95404beab730d47549bf3540c81df2b
Chukwubuikem Echeta <echetachukwubuikem@gmail.com>
2024-02-01 13:33:10 +0100

    Added lots of programs and files this time around. Well done, but note this is not best practise.

---
+++ intro-python/parsing-json/feedmeip.py
@@ -0,0 +1,7 @@
+has_ipaddr = True
+
+if has_ipaddr == True:
+    print("Device has an ip address.")
+
+else:
+    print("Assign device an ip address.")
---
+++ intro-python/types-loops-tools/week-day-for-loop.py
@@ -0,0 +1,12 @@
+weekDays = {"day1":"Sunday",
+            "day2":"Monday",
+            "day3":"Tuesday",
+            "day4":"Wednesday",
+            "day5":"Thursday",
+            "day6":"Friday",
+            "day7":"Saturday"}
+
+# for dayNumber, dayName in weekDays.items():
+#     print(f"{dayNumber} is {dayName}")
+
+print(dir(weekDays))
