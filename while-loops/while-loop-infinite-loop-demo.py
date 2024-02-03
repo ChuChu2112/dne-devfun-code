5797f1e9d6f49d5923d5f8fbe834073e6941729e
Chukwubuikem Echeta <echetachukwubuikem@gmail.com>
2024-02-03 18:59:57 +0100

    put try-except code into [counter function]
    inserted input variable, requesting [firstNumber] from user

--- while-loops/while-loop-infinite-loop-demo.py
+++ while-loops/while-loop-infinite-loop-demo.py
@@ -1,14 +1,17 @@
 from time import sleep
 
-number = 1
-try:        #try this
-    while True:     #infinite loop
-        try:        #try this
-            print(number)       #print current value of [number] starting with 1
-            number += 1     #incrementally add 1
-            sleep(1)        #delay 1 second
-        except KeyboardInterrupt:       #if there is a keyboard interruption
-            break       #stop
-finally:        #when original try loop end, 'finally' try this
-    print(f'''You stopped at {number - 1}. Way to go!''')       #print output
-    
+firstNumber = int(input("Which number would you like to start counting from: "))
+
+def counter(firstNumber):
+    try:        #try this
+        while True:     #infinite loop
+            try:        #try this
+                print(firstNumber)       #print current value of [number] starting with 1
+                firstNumber += 1     #incrementally add 1
+                sleep(1)        #delay 1 second
+            except KeyboardInterrupt:       #if there is a keyboard interruption
+                break       #stop
+    finally:        #when original try loop end, 'finally' try this
+        print(f'''You stopped at {firstNumber - 1}. Way to go!''')       #print output
+        
+counter(firstNumber)
